0b9f103c5fc5610aff12707a0770fa7921984f99
M.Shiri <ipo.net>
2020-08-09 02:02:45 -0700

    toast library

---
+++ ToasterLibrary/src/main/java/com/faraz/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,11 @@
+package com.faraz.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+
+    fun shiri(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
