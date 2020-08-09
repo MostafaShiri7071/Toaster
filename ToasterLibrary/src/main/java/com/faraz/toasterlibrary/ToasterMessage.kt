5302d16bb44af5d65baacaf6ec3c1be741b8147e
M.Shiri <ipo.net>
2020-08-09 02:33:50 -0700

    toast library new

--- ToasterLibrary/src/main/java/com/faraz/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/faraz/toasterlibrary/ToasterMessage.kt
@@ -8,4 +8,8 @@ class ToasterMessage {
     fun shiri(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
+
+    fun shiriNew(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_LONG).show()
+    }
 }
